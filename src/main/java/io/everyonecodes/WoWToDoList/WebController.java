4de4ae8b9dbc40fa4c3a96171a2add37933710d0
Martin Bollin <martinbollin92@gmail.com>
2023-11-24 11:54:12 +0100

    oauth2 works now

--- src/main/java/io/everyonecodes/WoWToDoList/WebController.java
+++ src/main/java/io/everyonecodes/WoWToDoList/WebController.java
@@ -1,13 +1,24 @@
 package io.everyonecodes.WoWToDoList;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.util.Collections;
+import java.util.Map;
+
+
+@RestController
 public class WebController {
 
     private final CharacterService characterService;
@@ -27,11 +38,46 @@ public class WebController {
     }
 
     @GetMapping("/characters/{id}")
-    ModelAndView taskListFromCharacterId(@PathVariable Long id){
+    ModelAndView taskListFromCharacterId(@PathVariable Long id) {
 
         ModelAndView view = new ModelAndView("taskListCharacterId.html");
         view.addObject("task", taskService.findByCharacterId(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Character not found with id " + id)));
         return view;
+    }
+
+
+//    @GetMapping("/callback")
+//    public Map<String, Object> blizzardLogin(Authentication authentication) {
+//        if (authentication != null) {
+//            System.out.println("Authentication Type: " + authentication.getClass().getName());
+//            System.out.println("Principal: " + authentication.getPrincipal());
+//
+//            if (authentication instanceof OAuth2AuthenticationToken) {
+//                OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+//                return oauthToken.getPrincipal().getAttributes();
+//            } else {
+//                throw new IllegalStateException("Unexpected authentication type: " + authentication.getClass());
+//            }
+//        } else {
+//            throw new IllegalStateException("Authentication is null");
+//        }
+//    }
+
+
+//    @GetMapping("/callback")
+//    public String handleCallback(Authentication authentication) {
+//        if (authentication != null && authentication.isAuthenticated()) {
+//            OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+//            System.out.println("Benutzerdetails im Callback: " + oauth2User.getAttributes());
+//            return "Erfolgreicher Callback!";
+//        } else {
+//            // Handle den Fall, wenn die Authentifizierung fehlschlägt
+//            return "Fehlerhafte Authentifizierung!";
+//        }
+//    }
+    @GetMapping("/callback")
+    public Map<String, Object> currentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        return oAuth2AuthenticationToken.getPrincipal().getAttributes();
     }
 }
---
+++ src/main/java/io/everyonecodes/WoWToDoList/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.everyonecodes.WoWToDoList.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/callback").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2login -> oauth2login
+                        .defaultSuccessUrl("/callback")
+                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig
+                                .baseUri("/oauth2/authorization")));
+        return http.build();
+    }
+}
