aebfa7610fb526c5d1f6b3da769f738abb447dbf
Martin Bollin <martinbollin92@gmail.com>
2023-12-21 10:38:11 +0100

    added name and description to swagger ui documentation

--- src/main/java/io/everyonecodes/WoWToDoList/character/WarcraftCharacterController.java
+++ src/main/java/io/everyonecodes/WoWToDoList/character/WarcraftCharacterController.java
@@ -1,5 +1,8 @@
 package io.everyonecodes.WoWToDoList.character;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,33 +22,39 @@ public class WarcraftCharacterController {
     }
 
     @GetMapping
+    @Operation(summary = "Get All Characters", description = "Retrieves a list of all Warcraft characters.")
     List<WarcraftCharacter> findAll() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get Character by ID", description = "Retrieves details of a specific Warcraft character based on the provided ID.")
     Optional<WarcraftCharacter> findById(@PathVariable Long id) {
         return Optional.of(service.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Character not found with id " + id)));
     }
 
     @PostMapping
+    @Operation(summary = "Create Character", description = "Creates a new Warcraft character.")
     WarcraftCharacter createCharacter(@RequestBody WarcraftCharacter character) {
         character.setFavorite(false);
         return service.createCharacter(character);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update Character by ID", description = "Updates the details of a specific Warcraft character based on the provided ID.")
     WarcraftCharacter updateCharacter(@RequestBody WarcraftCharacter character, @PathVariable Long id) {
         return service.updateCharacter(id, character);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Character by ID", description = "Deletes a specific Warcraft character based on the provided ID.")
     void deleteById(@PathVariable Long id) {
         service.deleteById(id);
     }
 
     @PutMapping("/{id}/favorite")
+    @Operation(summary = "Update Favorite Status", description = "Updates the favorite status of a specific Warcraft character based on the provided ID.")
     public ResponseEntity<WarcraftCharacter> updateFavoriteStatus(@PathVariable Long id, @RequestBody WarcraftCharacter character) {
         WarcraftCharacter updatedCharacter = service.updateFavoriteStatus(id, character.isFavorite());
         return new ResponseEntity<>(updatedCharacter, HttpStatus.OK);
--- src/main/java/io/everyonecodes/WoWToDoList/task/TaskController.java
+++ src/main/java/io/everyonecodes/WoWToDoList/task/TaskController.java
@@ -1,6 +1,7 @@
 package io.everyonecodes.WoWToDoList.task;
 
 import io.everyonecodes.WoWToDoList.character.WarcraftCharacter;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,38 +21,45 @@ public class TaskController {
     }
 
     @GetMapping
+    @Operation(summary = "Get All Tasks", description = "Retrieves a list of all tasks.")
     List<Task> findAll() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get Task by ID", description = "Retrieves details of a specific task based on the provided ID.")
     Optional<Task> findById(@PathVariable Long id){
         return Optional.of(service.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Task not found with id " + id)));
     }
 
     @GetMapping("/character/{id}")
+    @Operation(summary = "Get Tasks by Character ID", description = "Retrieves a list of tasks associated with a specific character based on the provided ID.")
     public Optional<List<Task>> findByCharacterId(@PathVariable Long id) {
         return Optional.of(service.findByCharacterId(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Character not found with id " + id)));
     }
 
     @PostMapping
+    @Operation(summary = "Create Task", description = "Creates a new task.")
     Task createTask(@RequestBody Task task) {
         return service.createTask(task);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update Task by ID", description = "Updates the details of a specific task based on the provided ID.")
     Task updateTask(@RequestBody Task updateTask, @PathVariable Long id) {
         return service.updateTask(updateTask, id);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Task by ID", description = "Deletes a specific task based on the provided ID.")
     void deleteTask(@PathVariable Long id) {
         service.deleteById(id);
     }
 
     @PutMapping("/{id}/complete")
+    @Operation(summary = "Update Complete Status", description = "Updates the complete status of a specific task based on the provided ID.")
     public ResponseEntity<Task> updateCompleteStatus(@PathVariable Long id, @RequestBody Task task) {
         Task updatedTask = service.updateCompleteStatus(id, task.isCompleted());
         return new ResponseEntity<>(updatedTask, HttpStatus.OK);
